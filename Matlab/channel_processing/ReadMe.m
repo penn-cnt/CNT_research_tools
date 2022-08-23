630507d25b607e4eebe2bb0be1601f9f849ef8bc
Erin <erincconrad@gmail.com>
2022-08-23 11:36:16 -0400

    Added ReadMe to Matlab branch

---
+++ Matlab/channel_processing/ReadMe.m
@@ -0,0 +1,15 @@
+%% Matlab ReadMe
+
+%% Suggested order to implement code
+%{
+1. Import EEG
+>> data_import/download_ieeg_data
+
+2. Clean/homogenize channel names
+>> channel_processing/decompose_labels
+
+3. Find non-intracranial channels
+>> channel_processiing/find_non_intracranial
+
+
+%}
