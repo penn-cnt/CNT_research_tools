3165f00d2b81823c4ea11129604368049c5ec0dc
Zack Goldblum <zackgoldblum@gmail.com>
2023-10-05 15:44:33 -0400

    Update __init__.py

--- python/tools/__init__.py
+++ python/tools/__init__.py
@@ -1,8 +1,8 @@
-'''
+"""
 Init file for tools
-'''
+"""
 
-from .get_iEEG_data import get_iEEG_data
+from .process_iEEG_data import get_iEEG_data, clean_labels, find_non_ieeg
 from .gini import gini
 from .line_length import line_length
 from .pull_patient_localization import pull_patient_localization
@@ -11,5 +11,3 @@ from .pull_sz_starts import pull_sz_starts
 from .bandpower import bandpower
 from .movmean import movmean
 from .plot_iEEG_data import plot_iEEG_data
-from .clean_labels import clean_labels
-from .find_non_ieeg import find_non_ieeg
